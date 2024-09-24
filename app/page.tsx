2a35bae1a68c415016348640091dbceeb0f6ba43
hiagoCf19 <hiagoferreira@id.uff.br>
2024-09-23 23:56:35 -0300

    feat: create navigator mobile and desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,35 @@
 import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-import { MenuIcon } from "lucide-react";
+import SheetHeaderComponent from "./header/sheet-header";
+
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <header className="flex justify-between items-center">
+    <div className="">
+      <header className="flex justify-between items-center border-b border-primary p-2 md:p-4">
         <p>Project name</p>
-        <Sheet>
-          <SheetTrigger className="border p-2 rounded-lg border-primary hover:bg-primary"><MenuIcon /></SheetTrigger>
-          <SheetContent>
-            <SheetHeader>
-              <SheetTitle>Menu</SheetTitle>
-              <SheetDescription>
-                <Button variant={"link"} >
-                  Amamentação
-                </Button>
-                <Button variant={"link"} >
-                  Primeiros socorros
+        <nav className="flex space-x-4 hidden sm:block" >
 
-                </Button>
-                <Button variant={"link"} >
-                  Higiene do bebê
+          <Button variant={"link"} className="text-lg p-0 " >
+            Amamentação
+          </Button>
+          <Button variant={"link"} className="text-lg p-0"  >
+            Primeiros socorros
 
-                </Button>
-                <Button variant={"link"}>Sobre o projeto</Button>
-              </SheetDescription>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
+          </Button>
+          <Button variant={"link"} className="text-lg p-0"  >
+            Higiene do bebê
+
+          </Button>
+          <Button variant={"link"} className="text-lg p-0" >Sobre o projeto</Button>
+
+
+        </nav>
+        <SheetHeaderComponent />
       </header>
-      <main className="flex">
+      <main className="flex p-4">
         main
       </main>
-      <footer className=" absolute bottom-0 border-primary  border-t -mx-4 w-full p-4">
+      <footer className=" fixed bottom-0 border-primary  border-t  w-full p-4">
         footer
       </footer>
 
